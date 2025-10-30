4a8bb09d8cdf6009b8750afe2995eeaa957aea7b
ridathariq <153930862+ridathariq@users.noreply.github.com>
2025-10-30 22:33:18 +0530

    Add files via upload

---
+++ library.java
@@ -0,0 +1,40 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class library extends student {
+
+    /**
+     * Default constructor
+     */
+    public library() {
+    }
+
+    /**
+     * 
+     */
+    public string libraryid;
+
+    /**
+     * 
+     */
+    public string location;
+
+    /**
+     * 
+     */
+    public void ddbook() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void addauthors() {
+        // TODO implement here
+    }
+
+}
---
+++ student.java
@@ -0,0 +1,33 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class student {
+
+    /**
+     * Default constructor
+     */
+    public student() {
+    }
+
+    /**
+     * 
+     */
+    public int id;
+
+    /**
+     * 
+     */
+    public string name;
+
+    /**
+     * 
+     */
+    public void takebook() {
+        // TODO implement here
+    }
+
+}
